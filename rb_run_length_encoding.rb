b3719e6129bd8753d42f19dacfb637c30134cbbe
Alexei <alexeim763@gmail.com>
2018-05-06 01:02:08 -0500

    more refactor

--- rb_run_length_encoding.rb
+++ rb_run_length_encoding.rb
@@ -1,23 +1,22 @@
 require 'byebug'
 def encode string_encode
   string_decode = ""
-  temp_letter = ""
+  temp_letter = string_encode[i]
   count_letter = 1
 
   length_word = string_encode.length
   for i in (0..(length_word-1)) do
-    if i == 0
-      temp_letter = string_encode[i]
-    elsif i == (length_word-1)
+    if i == (length_word-1)
       status_last = temp_letter == string_encode[i]
       count_letter += 1 if status_last
       string_decode += "#{count_letter}#{temp_letter}"
-      string_decode += "1#{string_encode[i]}" unless status_last
+      string_decode += "#{string_encode[i]}" unless status_last
     else
       if temp_letter == string_encode[i]
         count_letter += 1
       else
-        string_decode += "#{count_letter}#{temp_letter}"
+        string_decode += "#{count_letter}#{temp_letter}" if count_letter > 1
+        string_decode += temp_letter if count_letter == 1
         count_letter = 1
       end
       temp_letter = string_encode[i]      
