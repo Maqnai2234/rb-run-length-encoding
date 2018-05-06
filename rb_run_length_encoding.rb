161857ee7616b33d22855e25acbc68b8a0b8ad77
Alexei <alexeim763@gmail.com>
2018-05-05 23:23:31 -0500

    encode finish, first version

--- rb_run_length_encoding.rb
+++ rb_run_length_encoding.rb
@@ -1,14 +1,35 @@
-def encode string
-    string_encode = ""
-    temp_letter = ""
-    count_letter = 0
-    string.each_char do |s|
-        
-    end
-    return string_encode
+require 'byebug'
+def encode string_encode
+  string_decode = ""
+  temp_letter = ""
+  count_letter = 1
+
+  length_word = string_encode.length
+  for i in (0..(length_word-1)) do
+    if i == 0
+      temp_letter = string_encode[i]
+    elsif i == (length_word-1)
+      if temp_letter == string_encode[i]
+        count_letter += 1
+        string_decode += "#{count_letter}#{temp_letter}"
+      else
+        string_decode += "#{count_letter}#{temp_letter}"
+        string_decode += "1#{string_encode[i]}"
+      end
+    else
+      if temp_letter == string_encode[i]
+        count_letter += 1
+      else
+        string_decode += "#{count_letter}#{temp_letter}"
+        count_letter = 1
+      end
+      temp_letter = string_encode[i]      
+    end 
+  end
+  return string_decode
 end
 
 def decode string
 end
 
-encode("AAAAABBBZZZZDDDDDDDIII")
+puts encode("AAAAABBBZZZZDDDDDDDIIIX")
--- spec/run_length_encoding_spec.rb
+++ spec/run_length_encoding_spec.rb
@@ -3,7 +3,10 @@ require './rb_run_length_encoding.rb'
 describe 'rb_run_length_encoding' do
     it "encode string AAAAABBBZZZZDDDDDDDIII return to 5A3B4Z7D3I" do
         expect(encode("AAAAABBBZZZZDDDDDDDIII")).to eq "5A3B4Z7D3I"
-    end 
+    end
+    it "encode string AAAAABBBZZZZDDDDDDDIIIX return to 5A3B4Z7D3I1X" do
+        expect(encode("AAAAABBBZZZZDDDDDDDIIIX")).to eq "5A3B4Z7D3I1X"
+    end  
     it "decode string 5A3B4Z7D3I return to AAAAABBBZZZZDDDDDDDIII" do
         expect(decode("5A3B4Z7D3I")).to eq "AAAAABBBZZZZDDDDDDDIII"
     end
