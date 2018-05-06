1ed1bdf9e3cc4eaee968551a5ec2729076583864
Alexei <alexeim763@gmail.com>
2018-05-06 01:35:39 -0500

    code refactor

--- rb_run_length_encoding.rb
+++ rb_run_length_encoding.rb
@@ -2,21 +2,16 @@ require 'byebug'
 def encode string_encode
   string_decode = ""
   count_letter = 1
-
-  length_word = string_encode.length
-  for i in (0..(length_word-1)) do
-    if i == 0
-      temp_letter = string_encode[i]
-      next
-    end
-    last_word = (i == (length_word-1))
+  length_word_index = (string_encode.length - 1)
+  temp_letter = string_encode[0]
+  for i in (1..(length_word_index)) do
     if temp_letter == string_encode[i]
       count_letter += 1
-      string_decode << encode_render(temp_letter, count_letter) if last_word
+      string_decode << encode_render(temp_letter, count_letter) if (i == (length_word_index))
     else
       string_decode << encode_render(temp_letter, count_letter)
       count_letter = 1
-      string_decode << encode_render(string_encode[i], count_letter) if last_word
+      string_decode << encode_render(string_encode[i], count_letter) if (i == (length_word_index))
     end
     temp_letter = string_encode[i]
   end
@@ -24,12 +19,10 @@ def encode string_encode
 end
 
 def encode_render(letter, count_letter)
-  if count_letter == 1
-    letter
-  elsif count_letter > 1
-    "#{count_letter}#{letter}"
-  end
+  count_letter == 1 ? letter : "#{count_letter}#{letter}"
 end
 
 def decode string
 end
+
+puts encode("AAABBSDASDASDER")
