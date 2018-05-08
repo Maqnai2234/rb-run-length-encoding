edad9413831f24ba92865ae9dea5c9dcf6afe4ed
Alexei <alexeim763@gmail.com>
2018-05-08 00:19:56 -0500

    change describe testing

--- spec/run_length_encoding_spec.rb
+++ spec/run_length_encoding_spec.rb
@@ -7,7 +7,7 @@ describe 'rb_run_length_encoding' do
     it "encode string AAAAABBBZZZZDDDDDDDIIIX return to 5A3B4Z7D3IX" do
         expect(encode("AAAAABBBZZZZDDDDDDDIIIX")).to eq "5A3B4Z7D3IX"
     end
-    it "encode string with no repeat ABCDEF return to ABCDEF" do
+    it "encode string when string doesn't repeat ABCDEF return to ABCDEF" do
         expect(encode("ABCDEF")).to eq "ABCDEF"
     end    
     it "decode string 5A3B4Z7D3I return to AAAAABBBZZZZDDDDDDDIII" do
